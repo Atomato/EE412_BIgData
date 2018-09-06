d37c39cdd911ecc8b8525e6cd2ade800113ed705
Keunhyung Chung <axdwa777@gmail.com>
2018-09-06 15:23:47 +0900

    changed hw0.py to select alphabet

--- HW0/to/hw0.py
+++ HW0/to/hw0.py
@@ -3,8 +3,10 @@ import sys
 from pyspark import SparkConf, SparkContext
 
 def select_alphabet(s):
-	if len(s) is not 0:
-		return s
+	if 64<ord(s[0])<91 or 96<ord(s[0])<123:
+		return True
+	else:
+		return False
 
 conf = SparkConf()
 sc = SparkContext(conf=conf)
@@ -12,10 +14,11 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1])
 
 words = lines.flatMap(lambda l: re.split(r'[^\w]+', l))
-
 words = words.filter(lambda l: len(l) != 0)
 
-pairs = words.map(lambda w: (w, 1))
+alphabets = words.filter(select_alphabet)
+
+pairs = alphabets.map(lambda w: (w, 1))
 
 counts = pairs.reduceByKey(lambda n1, n2: n1 + n2)
 
