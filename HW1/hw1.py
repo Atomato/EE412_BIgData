80d2d06db36b0807e2c7b897865635a978310b52
Keunhyung Chung <axdwa777@gmail.com>
2018-09-13 01:13:27 +0900

    modified hw1.py: ordered pseudo_friends

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -2,12 +2,15 @@ import re
 import sys
 from pyspark import SparkConf, SparkContext
 
-def make_pair(t):
+def make_pseudo_friends(t):
 	friends = t[1]
 	pairs = []
 	for i in range(0, len(friends)):
 		for j in range(i+1, len(friends)):
-			pairs.append([friends[i], friends[j], False])
+			if friends[i] < friends[j]:
+				pairs.append([friends[i], friends[j], False])
+			else:
+				pairs.append([friends[j], friends[i], False])
 	return pairs
 
 conf = SparkConf()
@@ -18,7 +21,7 @@ lines = sc.textFile(sys.argv[1])
 tuples = lines.map(lambda l: l.split('\t'))
 tuples = tuples.map(lambda l: (l[0], l[1].split(',')))
 
-pseudo_friends = tuples.flatMap(make_pair)
+pseudo_friends = tuples.flatMap(make_pseudo_friends)
 
 print(pseudo_friends.collect())
 for _ in range(3):
