3afb72ad41d76814ce298cf8fda8e6aa8c1045eb
Keunhyung Chung <axdwa777@gmail.com>
2018-09-13 02:55:00 +0900

    hw1.py: changed friends tuples form to ((n, m),(count,boolean))

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -8,9 +8,9 @@ def make_pseudo_friends(t):
 	for i in range(0, len(friends)):
 		for j in range(i+1, len(friends)):
 			if friends[i] < friends[j]:
-				pairs.append(((friends[i], friends[j]), False))
+				pairs.append(((friends[i], friends[j]), (1, False)))
 			else:
-				pairs.append(((friends[j], friends[i]), False))
+				pairs.append(((friends[j], friends[i]), (1, False)))
 	return pairs
 
 conf = SparkConf()
@@ -20,12 +20,10 @@ lines = sc.textFile(sys.argv[1])
 tuples = lines.map(lambda l: l.split('\t'))
 tuples = tuples.map(lambda l: (l[0], l[1].split(',')))
 
-real_friends = tuples.flatMap(lambda l: [[l[0], l[1][i]] for i in range(len(l[1]))])\
-				.map(lambda l: ((l[0], l[1]), True) if l[0]<l[1] else ((l[1], l[0]), True))\
-				.reduceByKey(lambda b1, b2: b1 or b2)
-# real_friends = real_friends.map(lambda l: ((l[0], l[1]), True) if l[0]<l[1] \
-					# else ((l[1], l[0]), True))
-# real_friends = real_friends.reduceByKey(lambda b1, b2: b1 or b2)
+real_friends = tuples.flatMap(lambda l: [[l[0], l[1][i]] for i in range(len(l[1]))])
+real_friends = real_friends.map(lambda l: ((l[0], l[1]), (0, True)) if l[0]<l[1] \
+					else ((l[1], l[0]), (0, True)))
+real_friends = real_friends.reduceByKey(lambda b1, b2: (b1[0] + b2[0], b1[1] or b2[1]))
 
 pseudo_friends = tuples.flatMap(make_pseudo_friends)
 # pseudo_friends = pseudo_friends.union(real_friends)\
