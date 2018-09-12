8659c775fcb70075f109f4841862bb472a3db4b1
Keunhyung Chung <axdwa777@gmail.com>
2018-09-13 00:23:18 +0900

    make tuple of possible freinds

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -2,13 +2,24 @@ import re
 import sys
 from pyspark import SparkConf, SparkContext
 
+def make_pair(t):
+	freinds = t[1]
+	pairs = []
+	for i in range(0, len(freinds)):
+		for j in range(i+1, len(freinds)):
+			pairs.append([freinds[i], freinds[j]])
+	return pairs
+
 conf = SparkConf()
 sc = SparkContext(conf=conf)
 
 lines = sc.textFile(sys.argv[1])
 
 tuples = lines.map(lambda l: l.split('\t'))
+tuples = tuples.map(lambda l: (l[0], l[1].split(',')))
 
-print(tuples.collect())
+f_tuples = tuples.flatMap(make_pair)
+
+print(f_tuples.collect())
 for _ in range(3):
 	print('\n')
