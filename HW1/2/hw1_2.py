a1115dc0734613b74b1b0edfb407c1cd9997a9fb
Keunhyung Chung <axdwa777@gmail.com>
2018-10-03 20:02:15 +0900

    hw1_2.py: done by triangular matrix count

--- HW1/2/hw1_2.py
+++ HW1/2/hw1_2.py
@@ -4,10 +4,12 @@ import re
 def id_to_n(id, prefix_char):
 	# translate item id to integer
 	prefix_index = prefix_char.index(id[:3])
-	return prefix_index * 100000 + int(item[3:])
+	return prefix_index * 100000 + int(id[3:])
 
 def tri_index(i, j, M):
-	return (i-1)*(M-i/2) + j - i - 1
+	i = float(i)
+	j = float(j)
+	return int((i-1)*(M-i/2) + j - i - 1)
 
 with open(sys.argv[1], 'r') as f:
 	data = f.readlines()
@@ -21,17 +23,13 @@ with open(sys.argv[1], 'r') as f:
 		for item in items:
 			if not prefix_char.has_key(item[:3]):
 				prefix_char[item[:3]] = 0
-	prefix_char = prefix_char.keys() # return prefix characters
+	prefix_char = prefix_char.keys() # return prefix characters to list
 	N = 100000 * len(prefix_char) # number of possible items
 
 	item_counts = [0 for _ in range(N)]
 	for basket in data:
 		items = basket[:-2].split(' ') # split by 'space', [:-2] for remove ' \n'
 		for item in items:
-			# prefix_index = prefix_char.index(item[:3])
-
-			# # translate item id to integer
-			# item_number = prefix_index * 100000 + int(item[3:])
 			item_counts[id_to_n(item, prefix_char)] += 1
 ###################################################
 	# between the passes
@@ -45,13 +43,31 @@ with open(sys.argv[1], 'r') as f:
 ###################################################
 	# second pass
 	# triangular matrix for storing frequent-items pair count
-	# tri_matrix = [0 for _ in range(M*(M-1)/2)]
-	# for basket in data:
-	# 	items = basket[:-2].split(' ') # split by 'space', [:-2] for remove ' \n'
-	# 	freq_items = [] # list for freqeunt items in the current basket
-	# 	for item in items:
-	# 		if 
+	tri_matrix = [0 for _ in range(M*(M-1)/2)]
+	for basket in data:
+		items = basket[:-2].split(' ') # split by 'space', [:-2] for remove ' \n'
+		freq_items = [] # list for freqeunt items in the current basket
 
+		# check whick items are frequent
+		for item in items:
+			if item_counts[id_to_n(item, prefix_char)] >= 200:
+				freq_items.append(item)
+
+		for i in range(len(freq_items)):
+			for j in range(i+1, len(freq_items)):
+				# new numbering (1 to M) for ith frequent item in the current basket
+				u = freq_table[id_to_n(freq_items[i], prefix_char)]
+				# new numbering (1 to M) for jth frequent item in the current basket
+				v = freq_table[id_to_n(freq_items[j], prefix_char)]
+
+
+				if u < v:
+					tri_matrix[tri_index(u, v, M)] += 1	
+				elif u > v:
+					tri_matrix[tri_index(v, u, M)] += 1	
+				else:
+					pass
+			
 	print('number of frequent items: ' + str(M))
 	# item_counts.sort()
 	# print(item_counts[-20:])
