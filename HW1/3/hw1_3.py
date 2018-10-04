b8b296fd856e0ff6bd3ab3e080ae51e91e50b812
Keunhyung Chung <axdwa777@gmail.com>
2018-10-04 12:10:22 +0900

    hw1_3.py: done by getting 3-shingles

--- HW1/3/hw1_3.py
+++ HW1/3/hw1_3.py
@@ -24,15 +24,25 @@ with open(sys.argv[1], 'r') as f:
 			# if not yet meet 'space'
 			article_id.append(item[j])
 
-
 	data = list(map(lambda d: d.lower(), data))
 	# print(data[0])
 
 	articles = list(map(lambda d: re.split(r'\s+', re.sub(r'[^a-z ]+', '', d)), data))
+	# print(articles[0])
 
-	print(articles[0])
+	# considering a shingle unit as an alphabetic character within a word (Case 1-A)
+	# articles-shingle pairs set
+	shingles_set = [[] for _ in range(len(articles))]
+	for i in range(len(articles)):
+		item = articles[i] # for each article
+		for word in item:
+			if len(word) >= 3:
+				for j in range(len(word)-2):
+					# append 3-characters shingle to ith article set
+					shingles_set[i].append(word[j:j+3])
 
 
+	print(shingles_set[1])
 	# for pair in sorted(list(id_to_number.items()), key=operator.itemgetter(1))[:10]:
 	# 	print(pair)
 
