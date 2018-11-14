c5c2385848287978f8c9452186d821f90faa9d66
Keunhyung Chung <axdwa777@gmail.com>
2018-11-14 11:12:18 +0900

    hw3_1: made triples (src, (1, dst))

--- HW3/1/hw3_1.py
+++ HW3/1/hw3_1.py
@@ -8,8 +8,11 @@ sc.setLogLevel('WARN') # skip terminal log to see output well
 lines = sc.textFile(sys.argv[1]) # read graph.txt
 # print(lines.take(1))
 tuples = lines.map(lambda l: l.split('\t')) # split by tab, [src, dst]
-print(tuples.take(2))
-
+# print(tuples.take(2))
+triples = tuples.map(lambda l: (l[0],(1, l[1]))) # (source, (degree, destination))
+# print(triples.take(2))
+triples = triples.reduceByKey(lambda v1, v2: (v1[0] + v2[0], (v1[1], v2[1])))
+print(triples.take(1))
 
 # 					# get the top-10 pairs
 # ordered_output = sc.parallelize(ordered_output.take(10))\
