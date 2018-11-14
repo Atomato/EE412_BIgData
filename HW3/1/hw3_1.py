3356c29d2ff075001f5eca6d31cd9c4d6cb62bf1
Keunhyung Chung <axdwa777@gmail.com>
2018-11-14 10:34:13 +0900

    hw3_1: read 'graph.txt'

--- HW3/1/hw3_1.py
+++ HW3/1/hw3_1.py
@@ -5,3 +5,18 @@ conf = SparkConf()
 sc = SparkContext(conf=conf)
 sc.setLogLevel('WARN') # skip terminal log to see output well
 
+lines = sc.textFile(sys.argv[1]) # read graph.txt
+# print(lines.take(1))
+tuples = lines.map(lambda l: l.split('\t')) # split by tab, [src, dst]
+print(tuples.take(2))
+
+
+# 					# get the top-10 pairs
+# ordered_output = sc.parallelize(ordered_output.take(10))\
+# 					.map(lambda l: str(l[0][0])+'\t'+str(l[0][1]))
+# 					# formatting like <PAGE_ID><TAB><SCORE>
+
+# for s in ordered_output.collect():
+# 	print(s)
+
+sc.stop()					
