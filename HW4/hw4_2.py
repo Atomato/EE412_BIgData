a437661f2d3e88e74806d33bfb8229d8c390027f
Keunhyung Chung <axdwa777@gmail.com>
2018-12-13 04:44:33 +0900

    hw4_2.py: done

--- HW4/hw4_2.py
+++ HW4/hw4_2.py
@@ -1,73 +1,90 @@
-# python 2.7
 import sys
+from pyspark import SparkConf, SparkContext
+import time
 
-'''
-	dot product
-	https://stackoverflow.com/questions/32669855/dot-product-of-two-lists-in-python
-'''
-def dot(K, L):
-	if len(K) != len(L):
-		return 0
-	return sum(i[0] * i[1] for i in zip(K, L))
+conf = SparkConf()
+sc = SparkContext(conf=conf)
+sc.setLogLevel('WARN') # skip terminal log to see output well
 
-with open(sys.argv[1], 'r') as f, open(sys.argv[2], 'r') as l:
-	feat_e = f.readlines() # entire features
-	feat_e = list(map(lambda s: s.split(','),feat_e)) # split by comma
-	feat_e = list(map(lambda l: list(map(float,l)),feat_e)) # change to number
+# entire features
+feat_e = sc.textFile(sys.argv[1]).map(lambda s: s.split(','))\
+								.map(lambda l: list(map(float,l)))
+feat_e = feat_e.collect()
 
-	N_e = len(feat_e) # the number of data
-	dim = len(feat_e[0]) # features size
+lb_e = sc.textFile(sys.argv[2]).map(lambda l: float(l)) # entire labels
+lb_e = lb_e.collect()
 
-	lb_e = l.readlines() # entire labels
-	lb_e = list(map(float, lb_e)) # change to number
+N_e = len(feat_e) # the number of data
+dim = len(feat_e[0]) # features size
+acc_test = []
+for cv in range(10):
+	test_n = N_e/10 # size of test chunk
+	N = N_e - test_n # size of train chunks
 
-	acc_test = []
-	for cv in range(10):
-		test_n = N_e/10 # size of test chunk
-		test_f = feat_e[(test_n*cv):(test_n*cv) + test_n] # test features
-		test_l = lb_e[(test_n*cv):(test_n*cv) + test_n] # test labels
+	test_f = feat_e[(test_n*cv):(test_n*cv) + test_n] # test features
+	test_l = lb_e[(test_n*cv):(test_n*cv) + test_n] # test labels
 
-		feat = feat_e[:]
-		feat[(test_n*cv):(test_n*cv) + test_n] = [] # train features
-		lb = lb_e[:]
-		lb[(test_n*cv):(test_n*cv) + test_n] = [] # train labels
-		# print('lb size', len(lb))
+	# ((data idx i, dimension idx j), feature[i][j]), remove if feature[i][j] is 0	
+	test = sc.parallelize(range(test_n)) \
+				.flatMap(lambda i: [((i,j), test_f[i][j]) for j in range(dim)]) \
+				.filter(lambda l: l[1] != 0)
 
-		N = N_e - test_n # size of train chunks
+	feat = feat_e[:]
+	feat[(test_n*cv):(test_n*cv) + test_n] = [] # train features
+	lb = lb_e[:]
+	lb[(test_n*cv):(test_n*cv) + test_n] = [] # train labels	
 
-		w = [1./dim for _ in range(dim)]
-		b = -1.
+	# ((data idx i, dimension idx j), feature[i][j]), remove if feature[i][j] is 0
+	train = sc.parallelize(range(N)) \
+				.flatMap(lambda i: [((i,j), feat[i][j]) for j in range(dim)]) \
+				.filter(lambda l: l[1] != 0)
 
-		c = 0.2 # penalty of misclassification
-		lr = 0.0005 # learning rate eta
+	# print('count', train.count())
 
-		# train
-		for k in range(50):
-			w_n = w # new w
-			margin = [lb[j]*(dot(feat[j],w)+b) for j in range(N)]
+	w = [1./dim for _ in range(dim)]
+	b = -1.
 
-			for i in range(len(w)):
-				# gradient
-				gd = w[i] + c*sum([(0 if margin[j] >= 1 \
-							else -lb[j]*feat[j][i]) for j in range(N)])
-				w_n[i] = w_n[i] - lr*gd
+	c = 0.2 # penalty of misclassification
+	lr = 0.0005 # learning rate eta	
 
-			gd = c*sum([(0 if margin[j] >= 1 else -lb[j]) for j in range(N)])
-			b = b - lr*gd
-			w = w_n
+	# train
+	for k in range(50):
+		cur_time = time.time()
 
-			acc = sum([(1. if margin[j] > 0 else 0.) for j in range(N)])/N
-			w_len = sum(list(map(lambda x: x*x, w)))
-			cost = 0.5*w_len + c*sum(list(map(lambda x: max(0, 1-x), margin)))
-			# print('iteration', k,'accuracy:', acc, 'cost:', cost)
+		# (data idx i, lb[i]*(dot(feat[i],w)+b))
+		margin = train.map(lambda l: (l[0][0], l[1]*w[l[0][1]])) \
+					.reduceByKey(lambda v0, v1: v0 + v1) \
+					.map(lambda l: (l[0], lb[l[0]]*(l[1]+b)))
+					
+		# (dimension index j, gradient)
+		gd  = margin.filter(lambda l: l[1] < 1) \
+				.flatMap(lambda l: [(j, -lb[l[0]]*feat[l[0]][j]) for j in range(dim)] \
+																	+[(dim, -lb[l[0]])]) \
+				.reduceByKey(lambda v0, v1: v0 + v1) \
+				.map(lambda l: (l[0], w[l[0]] + c*l[1]) if l[0]<dim \
+															else ((l[0], c*l[1]))) 
+		# calculate new w and b
+		w_n = gd.map(lambda l: (l[0], w[l[0]] - lr*l[1]) if l[0]<dim \
+														else ((l[0], b - lr*l[1]))) \
+				.sortByKey(True).map(lambda l: l[1]).collect()
 
-		margin_test = [test_l[j]*(dot(test_f[j],w)+b) for j in range(test_n)]
-		temp = sum([(1. if margin_test[j] > 0 else 0.) for j in range(test_n)])/test_n
-		acc_test.append(temp)
+		# update
+		w = w_n[:dim]
+		b = w_n[dim]
 
-	# for acc in acc_test:
-	# 	print('test accuracy', acc)
+		acc = margin.map(lambda l: 1./N if l[1]>0 else 0.) \
+					.reduce(lambda a, b: a+b)
+		print('iteration', k, '%.3f seconds'%(time.time()-cur_time), 'accuracy:', acc)
 
-	print(sum(acc_test)/10)
-	print(c)
-	print(lr)
+	margin_test = test.map(lambda l: (l[0][0], l[1]*w[l[0][1]])) \
+					.reduceByKey(lambda v0, v1: v0 + v1) \
+					.map(lambda l: (l[0], test_l[l[0]]*(l[1]+b)))
+	
+	temp = margin_test.map(lambda l: 1./test_n if l[1]>0 else 0.) \
+					.reduce(lambda a, b: a+b)
+	print('%d fold test accuracy:'%cv, temp)
+	acc_test.append(temp)
+
+print(sum(acc_test)/10)
+print(c)
+print(lr)
