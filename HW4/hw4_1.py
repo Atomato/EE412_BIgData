c877f73c7afbd73c60ec2ebacd6734e855003aec
Keunhyung Chung <axdwa777@gmail.com>
2018-12-12 10:25:17 +0900

    hw4_1.py: done by no k fold

--- HW4/hw4_1.py
+++ HW4/hw4_1.py
@@ -1,3 +1,4 @@
+# python 2.7
 import sys
 
 '''
@@ -9,93 +10,52 @@ def dot(K, L):
 		return 0
 	return sum(i[0] * i[1] for i in zip(K, L))
 
-with open(sys.argv[1], 'r') as f:
-	with open(sys.argv[2], 'r') as l:
-		feat = f.readlines() # features
-		feat = list(map(lambda s: s.split(','),feat)) # split by comma
-		feat = list(map(lambda l: list(map(float,l)),feat)) # change to integer
-		N = len(feat) # the number of data
+with open(sys.argv[1], 'r') as f, open(sys.argv[2], 'r') as l:
+	feat = f.readlines() # features
+	feat = list(map(lambda s: s.split(','),feat)) # split by comma
+	feat = list(map(lambda l: list(map(float,l)),feat)) # change to number
+	N = len(feat) # the number of data
+	dim = len(feat[0]) # features size
 
-		lb = l.readlines() # labels
-		lb = list(map(int, lb)) # change to integer
+	lb = l.readlines() # labels
+	lb = list(map(float, lb)) # change to number
 
-		# temp = list(map(lambda x: (x + 1)/2., lb))
-		# print('temp', sum(temp)/N)
+	# temp = sum([sum(feat[j]) for j in range(N)])/(N*len(feat[0]))
+	# print('initail data', temp) # initail data distribution
 
-		# print('len feat', len(feat[0]))
-		# print('feat[0]', feat[0])
-		# print('len label', len(lb))
-		# print('label[:10]', lb[:10])
+	# temp = list(map(lambda x: (x + 1)/2., lb))
+	# print('initail label', sum(temp)/N) # initail label distribution
 
-		w = [0. for _ in range(len(feat[0]))]
-		b = 0.
+	# print('len feat', len(feat[0]))
+	# print('feat[0]', feat[0])
+	# print('len label', len(lb))
+	# print('label[:10]', lb[:10])
+	# print('data num', N)
 
-		c = 1 # penalty of misclassification
-		lr = 0.05 # learning rate eta
+	w = [1./dim for _ in range(dim)]
+	b = -1.
 
-
-
-		for k in range(50):
-			print('iteration',k)
-			w_n = w # new w
-			margin = [lb[j]*(dot(feat[j],w)+b) for j in range(N)]
-
-			for i in range(len(w)):
-				# gradient
-				gd = w[i] + c*sum([(0 if margin[j] >= 1 \
-							else -lb[j]*feat[j][i]) for j in range(N)])
-				w_n[i] = w_n[i] - lr*gd
-
-			gd = c*sum([(0 if margin[j] >= 1 else -lb[j]) for j in range(N)])
-			b = b - lr*gd
-			w = w_n
-
-			acc = sum([(1. if margin[j] > 0 else 0.) for j in range(N)])/N
-			print('accuracy:', acc)
+	c = 0.2 # penalty of misclassification
+	lr = 0.0005 # learning rate eta
 
 
-# x = np.array([[3., 4., 5.],\
-# 			[2., 7., 2.],\
-# 			[5., 5., 5.],\
-# 			[1., 2., 3.],\
-# 			[3., 3., 2.],\
-# 			[2., 4., 1.]])
 
-# y = np.array([1., 1., 1., -1., -1., -1.])
+	for k in range(50):
+		print('iteration',k)
+		w_n = w # new w
+		margin = [lb[j]*(dot(feat[j],w)+b) for j in range(N)]
 
-# w = np.array([1., 1., 1.])
-# b = -10.
+		for i in range(len(w)):
+			# gradient
+			gd = w[i] + c*sum([(0 if margin[j] >= 1 \
+						else -lb[j]*feat[j][i]) for j in range(N)])
+			w_n[i] = w_n[i] - lr*gd
 
-# c = 0.2 # penalty of misclassification
+		gd = c*sum([(0 if margin[j] >= 1 else -lb[j]) for j in range(N)])
+		b = b - lr*gd
+		w = w_n
 
-# w_len = sum(w*w)
-# margin = [y[j]*(sum(x[j]*w)+b) for j in range(len(x))]
-# print('Initial |w|: '+str(np.sqrt(w_len)))
-# for j in range(len(x)):
-# 	print('Initial y(wx+b) '+str(j)+': '+str(margin[j]))
-# print('Initial cost: '+str(0.5*w_len + c*sum(max([0. for _ in range(len(x))], margin))))
-
-# lr = 0.1 # learning rate
-# for k in range(100000):
-# 	print('\nIteration'+str(k))
-
-# 	w_n = w # new w
-# 	for i in range(len(w)):
-# 		# gradient
-# 		gd = w[i] + c*sum([(0 if y[j]*(sum(x[j]*w)+b) >= 1 else -y[j]*x[j][i]) \
-# 														for j in range(len(x))])
-# 		w_n[i] = w_n[i] - lr*gd
-
-# 	gd = c*sum([(0 if y[j]*(sum(x[j]*w)+b) >= 1 else -y[j]) for j in range(len(x))])
-# 	b = b - lr*gd
-# 	w = w_n
-
-# 	print('w: '+str(w))
-# 	print('b: '+str(b))
-
-# 	w_len = sum(w*w)
-# 	margin = [y[j]*(sum(x[j]*w)+b) for j in range(len(x))]
-# 	print('|w|: '+str(np.sqrt(w_len)))
-# 	for j in range(len(x)):
-# 		print('y(wx+b) '+str(j)+': '+str(margin[j]))
-# 	print('cost: '+str(0.5*w_len + c*sum(max([0. for _ in range(len(x))], margin))))	
+		acc = sum([(1. if margin[j] > 0 else 0.) for j in range(N)])/N
+		w_len = sum(list(map(lambda x: x*x, w)))
+		cost = 0.5*w_len + c*sum(list(map(lambda x: max(0, 1-x), margin)))
+		print('accuracy:', acc, 'cost:', cost)	
