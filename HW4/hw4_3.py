8932b1b037259b86de8cf92f33bda6cb651a3ace
Keunhyung Chung <axdwa777@gmail.com>
2018-12-14 05:53:08 +0900

    hw4_3.py: deleted "bucket"

--- HW4/hw4_3.py
+++ HW4/hw4_3.py
@@ -10,12 +10,11 @@ with open(sys.argv[1], 'r') as f:
 	k_list = sys.argv[2:] # list of 'k_i' 
 	k_list = list(map(int, k_list)) # change to integer
 
-	buckets = {} # key: end time, value: size
 	sizes = {} # key: size, value: [end times]
 	for i in range(N):
 		# if new bit is 1
 		if stream[i] == 1:			
-			buckets[i] = 1
+			# buckets[i] = 1
 			if 1 in sizes:
 				sizes[1].append(i) # append end time
 			else:
@@ -25,9 +24,6 @@ with open(sys.argv[1], 'r') as f:
 				# if there are 3 buckets of 'size'
 				if len(times) == 3:
 					# combine two leftmost buckets
-					buckets[times[1]] = 2 * size
-					del buckets[times[0]]
-
 					if 2 * size in sizes:
 						sizes[2 * size].append(times[1])
 					else:
@@ -35,33 +31,39 @@ with open(sys.argv[1], 'r') as f:
 					
 					del sizes[size][:-1]
 
-			# print('buckets', buckets)
+	# print buckets for debuging
+	# for size, times in sorted(sizes.items()):
+	# 	print(size, times)
 
-	for size, times in sorted(sizes.items()):
-		print(size, times)
-	# print('sizes', sizes[1], sizes[2], sizes[4], sizes[8], sizes[16], sizes[32], sizes[64], sizes[128], sizes[256])
-	# print('buckets', buckets)
+	# for debuging
+	# k_list = [N-2621313, N-2621314, N-2621315]
+
 	for k in k_list:
 		estimate = 0
-		temp = 0
+		temp = 0 # previous bucket size
 		for size, times in sorted(sizes.items()):
 			break_flag = False
 			for time in reversed(times):
-				time_idx = 0
 				if N-k < time:
 					estimate += temp
-					print('k', k, 'size', size, 'temp', temp, 'time', time, 'estimate', estimate, 'break flag', break_flag)
+					# print(N-k, 'time', time, 'size', size, 'temp', temp, 'estimate', estimate, 'break flag', break_flag)
 					temp = size
 				elif time == N-k: # corner case
 					estimate += temp + 1
 					break_flag = True
-					print('k', k, 'size', size, 'temp', temp, 'time', time, 'estimate', estimate, 'break flag', break_flag)
+					# print(N-k, 'time', time, 'size', size, 'temp', temp, 'estimate', estimate, 'break flag', break_flag)
 					break
 				else:
-					estimate += temp / 2 if temp > 1 else 1
+					estimate += 1 if temp == 1 else temp / 2
 					break_flag = True
-					print('k', k, 'size', size, 'temp', temp, 'time', time, 'estimate', estimate, 'break flag', break_flag)
+					# print(N-k, 'time', time, 'size', size, 'temp', temp, 'estimate', estimate, 'break flag', break_flag)
 					break
 			if break_flag:
-				break
+				break				
+		# if N-k < leftmost end time 
+		if break_flag == False:
+			estimate += 1 if temp == 1 else temp / 2
+			break_flag = True
+			# print(N-k, 'time', 0, 'size', 0, 'temp', temp, 'estimate', estimate, 'break flag', break_flag)			
+
 		print(int(estimate))
