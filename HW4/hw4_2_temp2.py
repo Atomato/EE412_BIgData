362c663fabec449ec880a6005afa125fc48e46b5
Keunhyung Chung <axdwa777@gmail.com>
2018-12-15 00:34:33 +0900

    hw4_2.py: use numpy

--- HW4/hw4_2_temp2.py
+++ HW4/hw4_2_temp2.py
@@ -1,7 +1,6 @@
 import sys
 from pyspark import SparkConf, SparkContext
 import time
-import numpy as np
 
 def dot(K, L):
 	if len(K) != len(L):
@@ -28,22 +27,19 @@ for cv in range(10):
 	N = N_e - test_n # size of train chunks
 
 	test_f = feat_e[(test_n*cv):(test_n*cv) + test_n] # test features
-	test_f = np.array(test_f)
 	test_l = lb_e[(test_n*cv):(test_n*cv) + test_n] # test labels
 
 	# (feature i, label i)
 	test = sc.parallelize(range(test_n)).map(lambda i: (test_f[i], test_l[i]))
 
 	feat = feat_e[:]
-	feat[(test_n*cv):(test_n*cv) + test_n] = [] 
-	feat = np.array(feat) # train features
+	feat[(test_n*cv):(test_n*cv) + test_n] = [] # train features
 	lb = lb_e[:]
 	lb[(test_n*cv):(test_n*cv) + test_n] = [] # train labels	
 
 	# (feature i, label i)
 	train = sc.parallelize(range(N)).map(lambda i: (feat[i], lb[i]))
 
-	# w = np.ones(dim) / dim
 	w = [1./dim for _ in range(dim)]
 	b = -1.
 
@@ -55,21 +51,21 @@ for cv in range(10):
 		cur_time = time.time()
 
 		# (feature, label, y(dot(x,w)+b))
-		margin = train.map(lambda (x, y): (x, y, y*(np.dot(x,w)+b)))
+		margin = train.map(lambda (ft, lb): (ft, lb, lb*(dot(ft, w)+b)))
 	
-		# [(gradients for w), gradient for b]
-		gd = margin.filter(lambda (x, y, margin): margin < 1) \
-			.map(lambda (x, y, margin): np.append(-y*x,-y)) \
-			.reduce(lambda gd0, gd1: gd0 + gd1)
-		gd = np.append(w,0) + c*gd
+		# [gradients for w] + [gradient for b]
+		gd = margin.filter(lambda (ft, lb, margin): margin < 1) \
+			.map(lambda (ft, lb, margin): [-lb*ft[j] for j in range(dim)]+[-lb]) \
+			.reduce(lambda gd0, gd1: [gd0[j] + gd1[j] for j in range(dim+1)])
+		gd = [w[j] + c*gd[j] for j in range(dim)] + [c*gd[dim]]
 		
 		# update
-		w = w - lr*gd[:dim]
+		w = [w[j] - lr*gd[j] for j in range(dim)]
 		b = b - lr*gd[dim]
 
 		# print('iteration', k, '%.3f seconds'%(time.time()-cur_time))
 
-	margin_test = test.map(lambda (x, y): y*(np.dot(x,w)+b))
+	margin_test = test.map(lambda (ft, lb): lb*(dot(ft, w)+b))
 	temp = margin_test.map(lambda margin: 1./test_n if margin>0 else 0.) \
 					.reduce(lambda a, b: a+b)
 	# print('%d fold test accuracy:'%cv, temp)
